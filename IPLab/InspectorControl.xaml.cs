95e1d6d2d63167750f117bee813d2c19e6020860
Yoav Miller <yoav.miller@utilight.com>
2026-05-19 13:10:59 +0300

    WIP displaying circles in inspector

---
+++ IPLab/InspectorControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace IPLab;
+
+public partial class InspectorControl : UserControl
+{
+    public InspectorControl()
+    {
+        InitializeComponent();
+    }
+}
